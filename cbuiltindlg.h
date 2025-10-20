b5183ad5865ccb6ee8206d94338160f335d9dbc8
41343112 <41343112@nfu.edu.tw>
2025-10-20 16:23:11 +0800

    Project Create

---
+++ cbuiltindlg.h
@@ -0,0 +1,33 @@
+#ifndef CBUILTINDLG_H
+#define CBUILTINDLG_H
+
+#include <QDialog>
+#include<QPushButton>
+#include<QTextEdit>
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class CBuiltinDlg;
+}
+QT_END_NAMESPACE
+
+class CBuiltinDlg : public QDialog
+{
+    Q_OBJECT
+
+public:
+    CBuiltinDlg(QWidget *parent = nullptr);
+    ~CBuiltinDlg();
+
+private:
+    Ui::CBuiltinDlg *ui;
+    QTextEdit   *displayTextEdit;
+    QPushButton *colorPushBtn;
+    QPushButton *errorPushBtn;
+    QPushButton *filePushBtn;
+    QPushButton *fontPushBtn;
+    QPushButton *inputPushBtn;
+    QPushButton *pagePushBtn;
+    QPushButton *progressPushBtn;
+    QPushButton *printPushBtn;
+};
+#endif // CBUILTINDLG_H
