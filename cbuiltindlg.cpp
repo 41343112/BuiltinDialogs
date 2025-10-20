51d1bcbe97f8f94d96fafcf4d8f78e7685431815
41343112 <41343112@nfu.edu.tw>
2025-10-20 17:08:21 +0800

    update

--- cbuiltindlg.cpp
+++ cbuiltindlg.cpp
@@ -11,10 +11,7 @@ CBuiltinDlg::CBuiltinDlg(QWidget *parent)
     errorPushBtn    =new QPushButton(QStringLiteral("錯誤對話盒"));
     filePushBtn     =new QPushButton(QStringLiteral("檔案對話盒"));
     fontPushBtn     =new QPushButton(QStringLiteral("字體對話盒"));
-    inputPushBtn    =new QPushButton(QStringLiteral("輸入對話盒"));
-    pagePushBtn     =new QPushButton(QStringLiteral("頁面設定對話盒"));
-    progressPushBtn =new QPushButton(QStringLiteral("進度對話盒"));
-    printPushBtn    =new QPushButton(QStringLiteral("列印對話盒"));
+
     ui->setupUi(this);
 }
 
